c0882142188c6f495f0c3f2bd6f2c38dfc201015
ppdx999 <ppdx999@gmail.com>
2022-01-16 20:40:32 +0900

    Add example for AllNoResetIO

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { IO, next } from "./lib/ReactiveMonad";
+import { allNoResetIO } from "./lib/AllNoResetIO";
 
 const rootElement = document.getElementById("root");
-console.log(rootElement);
 
-const count = IO(0);
-const decrement = () => count[">"](next(count.lastVal - 1));
-const increment = () => count[">"](next(count.lastVal + 1));
+const countIO = IO(0);
+const decrement = () => countIO[">"](next(countIO.lastVal - 1));
+const increment = () => countIO[">"](next(countIO.lastVal + 1));
 
-const App = () => (
-  <div className="App">
-    <h1>Hello React FRP Monad</h1>
-    <h2>You clicked {count.lastVal} times!</h2>
-
+const Counter = () => (
+  <div>
+    <h2>You clicked {countIO.lastVal} times!</h2>
     <button onClick={decrement}>Decrement</button>
     <button onClick={increment}>Increment</button>
   </div>
 );
 
-const reactIO = count[">="](() => ReactDOM.render(<App />, rootElement));
+const start = Date.now();
+const getSecond = () => Math.floor((Date.now() - start) / 1000);
+const timerIO = IO(0);
+const f = () => timerIO[">"](next(getSecond()));
+setInterval(f, 1000);
+
+const Timer = () => <h2>{timerIO.lastVal}</h2>;
+
+const App = () => (
+  <div className="App">
+    <h1>React + FRP (Multi Components)</h1>
+    <Counter />
+    <Timer />
+  </div>
+);
+
+const allIO = allNoResetIO([countIO, timerIO]);
+
+const reactIO = allIO[">="](() => ReactDOM.render(<App />, rootElement));
